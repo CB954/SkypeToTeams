4259ebe1ac92630afdc8c763f7f3805a5eca6019
Chris Brannon <53869657+CB954@users.noreply.github.com>
2019-09-04 14:12:18 -0400

    Update SkypeToTeams.ps1

--- SkypeToTeams.ps1
+++ SkypeToTeams.ps1
@@ -12,25 +12,31 @@
 #>
 
 #Create/Modify Reg Keys
-New-Item "HKCU:\Software\Classes\skype\shell\open\command" -Force
-New-ItemProperty "HKCU:\Software\Classes\skype" -Name '(Default)' -Value 'URL:skype' -Force
-New-ItemProperty "HKCU:\Software\Classes\skype" -Name 'URL Protocol' -Force
-New-ItemProperty "HKCU:\Software\Classes\skype\shell\open\command" -Name '(Default)' -Value '"C:\SkypeToTeams\SkypeToTeams.bat" "%1"' -Force
+Write-Host -ForegroundColor Green "Modifying Registry"
+New-Item "HKCU:\Software\Classes\skype\shell\open\command" -Force | Out-Null
+New-ItemProperty "HKCU:\Software\Classes\skype" -Name '(Default)' -Value 'URL:skype' -Force | Out-Null
+New-ItemProperty "HKCU:\Software\Classes\skype" -Name 'URL Protocol' -Force | Out-Null
+New-ItemProperty "HKCU:\Software\Classes\skype\shell\open\command" -Name '(Default)' -Value '"C:\SkypeToTeams\SkypeToTeams.bat" "%1"' -Force | Out-Null
 
 #Make sure C:\SkypeToTeams folder exists
+Write-Host -ForegroundColor Green "Creating Directory C:\SkypeToTeams"
 if (!(Test-Path C:\SkypeToTeams)) {New-Item -Type Directory 'C:\SkypeToTeams'}
 
 #Create Bat file
+Write-Host -ForegroundColor Green "Creating .bat file"
 $BatFile = "@echo off
 set origNumber=%1
 set number=%origNumber:?call=%
 `"$env:USERPROFILE\AppData\Local\Microsoft\Teams\current\Teams.exe`" tel:%number%"
-$BatFile | Out-file -Encoding "ASCII" C:\SkypeToTeams\SkypeToTeamsPublic.bat
+$BatFile | Out-file -Encoding "ASCII" C:\SkypeToTeams\SkypeToTeams.bat
 
 #Create README File
+Write-Host -ForegroundColor Green "Creating README File"
 $ReadMeFile = "This is used to convert Skype URL calls to Microsoft Teams. It was written and tested for ConnectWise Manage.
 
 The bat file converts the skype call to one accepted by Teams. 
 
 https://github.com/CB954/SkypeToTeams"
 $ReadMeFile | Out-file -Encoding "ASCII" C:\SkypeToTeams\README.txt
+
+Write-Host -ForegroundColor Green "Completed..."
